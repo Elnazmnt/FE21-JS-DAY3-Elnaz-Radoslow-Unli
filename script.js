cd8baba463ebc399438c39858453a3cb54b7d318
RdkSki <rotax@gmx.net>
2021-09-27 11:17:15 +0200

    basic1

---
+++ script.js
@@ -0,0 +1,8 @@
+var x = Math.floor((Math.random() *30) -4);
+    console.log(x);
+
+    if (x < 11) {
+        console.log("the weather is cold");
+        } else {
+            console.log("the weather is moderate");
+        }
