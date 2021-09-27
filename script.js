b4b7ce05b47b0c2adbdd8c3ebaed8271d3fadac4
RdkSki <rotax@gmx.net>
2021-09-27 14:45:48 +0200

    fixing

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ console.log("highest value is: number " + hi);*/
 
 
 
-/*var x = Math.floor((Math.random() *55) -15);
+var x = Math.floor((Math.random() *55) -15);
     console.log(x);
     if (x < 11) {
         document.write("the weather is cold");
@@ -30,14 +30,31 @@ console.log("highest value is: number " + hi);*/
         } else {
             document.write("the weather is hot");
              document.getElementById("hot").style.display = "flex";
-        };*/
+        };
 
-let students = ["Martin", "Thomas", "Klaus", "Maria", "David"];
+/*let students = ["Martin", "Thomas", "Klaus", "Maria", "David"];
 let grades = [76, 85, 65, 93, 81];
 
-document.write(students[0], grades[0]);
-document.write(students[1], grades[1]);
-document.write(students[2], grades[2]);
-document.write(students[3], grades[3]);
-document.write(students[4], grades[4]);
+
+for (let i = 0; i < grades.length; i++){
+document.write(students[i] + "=" + grades[i] + "<br>");
+}
+
+let sum = 0;
+for (let f = 0; f < grades.length; f++){
+     sum=sum+grades[f];
+}
+let avg = sum/grades.length;
+
+switch (avg) {
+    case (avg < 60): "F";break;
+    case (avg < 70): "D";break;
+    case (avg < 80): "C";break;
+    case (avg < 90): "B";break;
+    default: ('Class average grade is: ' + 'A');break;
+}
+
+
+
+document.write('Class average grade is: ' + avg);*/
 
