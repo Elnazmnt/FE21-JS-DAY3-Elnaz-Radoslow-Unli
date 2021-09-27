16f6f04ee7c41287bf7cd5a3bdb187e18104ddfe
RdkSki <rotax@gmx.net>
2021-09-27 12:33:47 +0200

    basic1and2 final

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-var x = Math.floor((Math.random() *30) -4);
+/*var x = Math.floor((Math.random() *31) -5);
     console.log(x);
 
     if (x < 11) {
         console.log("the weather is cold");
         } else {
             console.log("the weather is moderate");
-        }
+        };*/
+
+
+/*let array = [1, 7, 55, -3, 9, 111];
+let hi= array[0];
+for(let i = 0; i < array.length; i++){
+       if(hi < array[i]){
+hi = array[i+1];
+    }
+}
+console.log("highest value is: number " + hi);*/
+
+
+var x = Math.floor((Math.random() *55) -15);
+    console.log(x);
+
+    if (x < 11) {
+        console.log("the weather is cold"); 
+        } else if (x < 26) {
+            console.log("the weather is moderate");
+        } else {
+            console.log("the weather is hot");
+        };
